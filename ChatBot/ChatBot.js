876fc7467cce0823d6eca0f66bb3b45150da3a8e
Atul Sahu <127933681+AtulSahu778@users.noreply.github.com>
2025-11-22 00:47:30 +0530

    learned about states eventhandlers

--- ChatBot/ChatBot.js
+++ ChatBot/ChatBot.js
@@ -1,10 +1,45 @@
+function ChatInput({chatMessages, setChatMessages}){
+    const [inputText, setInputText] = React.useState('');
+    function SaveInputText(event){
+        setInputText(event.target.value);
+    }
+
+    function sendMessage(event){
+        const newChatMessages = [
+        ...chatMessages, // copying the data by spreading out it
+        {
+            message: inputText,
+            sender: 'user',
+            id: crypto.randomUUID() // random id
+        }
+    ]
+        
+        setChatMessages(newChatMessages);
+       
+
+     const response = Chatbot.getResponse(inputText);
+        
+        setChatMessages([
+        ...newChatMessages, // copying the data by spreading out it
+        {
+            message: response,
+            sender: 'robot',
+            id: crypto.randomUUID() // random id
+        }
+    ]);
 
 
-function ChatInput(){
+        setInputText('');
+    }
     return (
        <>
-            <input placeholder="Send a message to Chat Bot " size={30}/>
-            <button>Send</button>
+            <input placeholder="Send a message to Chat Bot " size={30} 
+            onChange={SaveInputText}
+            value={inputText}
+            />
+            <button 
+             onClick={sendMessage}
+            >Send</button>
         </>
     );
 }
@@ -37,44 +72,67 @@ function ChatMessage({message,sender}){
     );
 }
 
- function App(){
 
 
-    const chatMessages = [{
-        message: 'hello chatbot',
-        sender: 'user',
-    },
-    {
-        message: 'Hello! How can i help you?',
-        sender: 'robot'
-    },
-    {
-        message: `Can you get me today's date?`,
-        sender: 'user'
-    },
-    {
-        message: `Today is 19 November`,
-        sender: 'robot'
-    }
-];
 
+ function ChatMessages({chatMessages}){
 
-  const chatMessageComponents = chatMessages.map((chatMessage) => {
-        return (
-            <ChatMessage 
-                message = {chatMessage.message}
-                sender = {chatMessage.sender}
-            />
-        )
-    });
+    
+    // const [chatMessages, setChatMessages] = array; // this is called array destructuring.
 
-
+    // const chatMessages = array[0]; // currrent data
+    // const setChatMessages = array[1]; // updater function which updates the current data by making a copy of that
 
 
     return (
     <>
-    <ChatInput/>
-    {chatMessageComponents}
+    {chatMessages.map((chatMessage) => {
+        return (
+            <ChatMessage 
+                message = {chatMessage.message}
+                sender = {chatMessage.sender}
+                key = {chatMessage.id}
+            />
+        )
+    })}
+    </>
+    );
+ }
+
+ function App(){
+
+    const [chatMessages, setChatMessages] = React.useState([{
+            message: 'hello chatbot',
+            sender: 'user',
+            id: 1
+        },
+        {
+            message: 'Hello! How can i help you?',
+            sender: 'robot',
+            id: 2
+        },
+        {
+            message: `Can you get me today's date?`,
+            sender: 'user',
+            id: 3
+        },
+        {
+            message: `Today is 19 November`,
+            sender: 'robot',
+            id: 4
+        }
+    ])
+
+
+    return (
+    <>
+    <ChatInput 
+    chatMessages = {chatMessages}
+    setChatMessages = {setChatMessages}
+    />
+    <ChatMessages 
+    chatMessages = {chatMessages}
+    />
     </>
     );
  }
