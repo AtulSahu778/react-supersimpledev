be00a226b4bae82ae463dd06cff7c6642ac37420
Atul Sahu <127933681+AtulSahu778@users.noreply.github.com>
2025-12-04 21:58:15 +0530

    update fetching data from .then() to async and await

--- ecommerce-project/src/pages/Orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/Orders/OrdersPage.jsx
@@ -11,10 +11,13 @@ function OrdersPage() {
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/orders?expand=products')
-     .then((response) => {
+
+    const fetchOrdersData = async () => {
+      const response = await axios.get('/api/orders?expand=products');
       setOrders(response.data);
-     })
+    }
+    
+    fetchOrdersData();
   }, []);
 
 
